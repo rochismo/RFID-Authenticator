9ff0297b4a5d1b708e17182db3857fc58b2dbe92
rmariscal <mariscal.rmc@gmail.com>
2019-02-20 20:06:11 +0100

    Metodo para verificar. Enviará petición al servidor de verificacion y controlará si le han enviado ok o error

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("hola");
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+
+const verificationServer = "http://localhost:3000/verify";
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+
+//Verify mock
+app.get('/verify', function (req, res) {
+    if(req.query.machineId == 1){
+        res.send("verificado")
+    } else {
+        res.send("no estas verificado")
+    }
+});
+
+
+
+//Send http request to verify signin
+function verifyRFID(rfid) {
+    axios.get(verificationServer, {
+        params: rfid
+    })
+        .then(response => {
+            console.log(response.data);
+            return response.data
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
