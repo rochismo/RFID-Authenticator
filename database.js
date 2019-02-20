c5b923a3f030e8b3d0ba2d8ad0be6618cb89b185
Javeir Viñas Garcia <javiervinasgarcia@gmail.com>
2019-02-20 20:42:01 +0100

    Añadadida database

---
+++ database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/signin');
+
+const Signing = mongoose.model('Signin', {
+    idMachine: String,
+    rfid: String,
+    date: String,
+    hour: String
+});
+
+module.exports.saveDatabase = function (Signin) {
+    console.log("Saving signin");
+    let signin = new Signing()
+    signin.idMachine = Signin.idMachine;
+    signin.rfid = Signin.rfid;
+    signin.date = Signin.date;
+    signin.hour = Signin.hour;
+    signin.save().then(_ => console.log("Signin saved!"));
+
+}
+
