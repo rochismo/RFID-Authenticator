f5d6f1ec51d608b0add4fd54194d2fb54dbd10f3
Javeir Viñas Garcia <javiervinasgarcia@gmail.com>
2019-02-20 20:42:51 +0100

    solucionando errores

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-
+const savedata = require('./database.js');
+//Conection with windows
 const serialWindowsPort = "COM3";
 const moment = require('moment');
 var idMachine = '192.168.1.1';
@@ -12,26 +13,6 @@ const port = new SerialPort(serialWindowsPort, {
 
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 
-
-// Reading the port data:
-port.on("open", () => {
-  console.log('Serial Port Opening');
-});
-
-// read the data of the rfid:
-parser.on('data', rfid => {
-
-  rfid = rfid.trim();
-
-  var date = moment().format("DD-MM-YYYY");
-  var hour = moment().format("HH:mm:ss");
-
-  var responseObj = new Response (rfid, date, hour, idMachine)
-
-  console.log(JSON.stringify(responseObj));
-
-});
-
 // Response Object
 function Response (rfid, date, hour, idMachine) {
     this.rfid = rfid;
@@ -39,6 +20,28 @@ function Response (rfid, date, hour, idMachine) {
     this.hour = hour;
     this.idMachine = idMachine;
 }
+
+// Reading the port data:
+port.on("open", () => {
+    console.log('Serial Port Opening');
+});
+
+// read the data of the rfid:
+parser.on('data', rfid => {
+
+    rfid = rfid.trim();
+
+    var date = moment().format("DD-MM-YYYY");
+    var hour = moment().format("HH:mm:ss");
+
+    var responseObj = new Response (rfid, date, hour, idMachine);
+
+    savedata.saveDatabase(responseObj);
+    console.log(JSON.stringify(responseObj));
+
+});
+
+
 
 const express = require('express');
 const app = express();
