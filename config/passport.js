6dfcb6e15ccbdf4a06bd0685dd3da5840e17150c
rochismo <alexis.platino@gmail.com>
2019-03-21 19:29:18 +0100

    Deleted index.js, removed some irrelevant else statements and refactored some code in routes.js and passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,53 +5,50 @@ const LocalStrategy = require('passport-local').Strategy;
 const fetch = require("node-fetch");
 
 //Deserializators
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
-  });
-    
-  passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-  });
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
 
-  //Strategy Config Google Auth
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+//Strategy Config Google Auth
 passport.use(new GoogleStrategy({
     clientID: constants.CLIENT_ID,
     clientSecret: constants.CLIENT_SECRET,
     callbackURL: constants.CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
-      return cb(null,profile);
+  function (accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
   }
 ));
 
 //Local Strategy, connect to login server
-passport.use(new LocalStrategy(
-  {usernameField: 'email',
-  passwordField: 'password'},
-   async function(userid, password, next) {
-    var date = JSON.stringify({
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  async function (userid, password, next) {
+    const date = JSON.stringify({
       "username": userid,
       "password": password
     });
 
     let userData = await requestLogin(date);
-      return next(null, userData);
-}));
+    return next(null, userData);
+  }));
 
 //Async function, send username and password to Login server an recibe a user
 async function requestLogin(infouser) {
-  let response = await fetch(constants.URL_LOGIN_GROC,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: infouser,
-    })
-    
-  let data = await response
-  if(data.status===200){
-    return await data.json();
-  }else{
-    return false;
-  }
+  let response = await fetch(constants.URL_LOGIN_GROC, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: infouser,
+  })
+
+  return response.status === 200 ? await response.json() : false;
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,63 +4,85 @@ const constants = require('../constants.js');
 const jwt = require('jsonwebtoken');
 const path = express.Router();
 
+function handleToken(err, decoded) {
+  if (err) {
+    return res.json({
+      success: false,
+      message: 'Failed to authenticate token.'
+    });
+  }
+  // if everything is good return this json
+  return res.json({
+    success: true,
+    message: 'Is verified.'
+  });
+
+}
+
 //End Point with verify Logic
 path.post('/verify', function (req, res) {
-
-  if (req.headers.authorization) {
-    let token = req.headers.authorization.split(" ")[1];
-    if (token) {
-      try {
-        jwt.verify(token, constants.SECRET_JWT, function(err, decoded) {      
-          if (err) {
-            return res.json({ success: false, message: 'Failed to authenticate token.' });    
-          } else {
-                // if everything is good return this json
-                return res.json({ success: true, message: 'Is verified.' });  
-              }
-            });
-          } catch (err) {
-          res.send(400);
-        }
-      } else {
-        res.send(400);
-      }
-  } else {
-    res.send(401);
+  if (!req.headers.authorization) {
+    // There is no auth code
+    res.statusCode(401).send(401);
+    return;
   }
-    
-    });
-  
-  //End Point with Google Auth
-  path.get('/google',
-    passport.authenticate('google', { scope: ['profile'] }));
-  
-  path.get('/google/callback', 
-    passport.authenticate('google',{
-      failureRedirect: "/auth/google",
-      
-      }),
-    function(req, res) {
-      let token = jwt.sign({
-        profile: req.user,
-    }, constants.SECRET_JWT, { expiresIn: constants.TOKEN_EXPIRE })
-    // Successful authentication, send Token.
-      res.json({ token: token });
-    });
-    
-    path.post('/local', passport.authenticate('local', { session: false }), function(req,res){
-      if(!req.user===false){
-        let token = jwt.sign({
-          profile : req.user
-        }, constants.SECRET_JWT, { expiresIn: constants.TOKEN_EXPIRE })
-    
-        //res.locals.token=token;
-        res.json({ token: token });
-      }else{
-        return res.json({ success: false, message: 'Failed to authenticate token.' });   
-      }
-     
-  });
-  module.exports = path;
 
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    // No token found
+    res.statusCode(400).send(400);
+    return;
+  }
+
+  // Verify token
+  jwt.verify(token, constants.SECRET_JWT, handleToken);
+
+});
+
+//End Point with Google Auth
+path.get('/google',
+  passport.authenticate('google', {
+    scope: ['profile']
+  }));
+
+path.get('/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: "/auth/google",
+
+  }),
+  function (req, res) {
+    let token = jwt.sign({
+      profile: req.user,
+    }, constants.SECRET_JWT, {
+      expiresIn: constants.TOKEN_EXPIRE
+    })
+    // Successful authentication, send Token.
+    res.json({
+      token: token
+    });
+  });
+
+path.post('/local', passport.authenticate('local', {
+  session: false
+}), function (req, res) {
+  if (!req.user) {
+    return res.json({
+      success: false,
+      message: 'Failed to authenticate token.'
+    });
+  }
   
+  const token = jwt.sign({
+    profile: req.user
+  }, constants.SECRET_JWT, {
+    expiresIn: constants.TOKEN_EXPIRE
+  })
+
+  //res.locals.token=token;
+  res.json({
+    token: token
+  });
+
+});
+
+module.exports = path;
